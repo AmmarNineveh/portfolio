c3996221522070310e8827402d93262cfee160f8
GG <acamirecam@gmail.com>
2026-01-29 13:44:16 +0300

    Add Discord verification + design polish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased paper-texture grid-pattern`}
       >
         {children}
       </body>
